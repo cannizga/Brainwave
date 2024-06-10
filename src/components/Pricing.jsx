2b19632723c979b738fbe7c3e613f8a0c5ffac71
cannizga <giuseppeacannizzojr@gmail.com>
2024-06-09 21:46:58 -0500

    Update

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Section from "./Section";
+
+const Pricing = () => {
+  return (
+    <Section className="overflow-hidden" id="pricing">
+      <div className="container relative z-2">
+        <div className="hidden relative justify-center mb-[6.5rem] lg:flex"></div>
+      </div>
+    </Section>
+  );
+};
+
+export default Pricing;
